adbf54a01687f44646b4c99ddf32ee372788a1d0
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-12-17 19:46:32 +0100

    Use React.Fragement as a wrapper in App Component

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
 const App = () => (
-  <div>
+  <>
     <h1>App Component</h1>
     <Display result="0" />
     <ButtonPanel />
-  </div>
+  </>
 );
 
 export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Display = ({ result }) => (
-  <div>
+  <>
     <p>{result}</p>
-  </div>
+  </>
 );
 
 Display.defaultProps = {
