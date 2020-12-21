cbe14574173c232bd5571742cd8b0a4c52b85618
Zakaraie El Mejdki <elmejdki.zakariae@gmail.com>
2020-12-21 23:25:57 +0100

    Add content to Quote Page

--- src/components/pages/QuotePage.js
+++ src/components/pages/QuotePage.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const QuotePage = () => (
-  <div>
-    <h1>Quote Page</h1>
+  <div className="max-height max-width quote">
+    <h1>Quote Of The Day:</h1>
+    <div>
+      <p>
+        <strong>Mathematics </strong>
+        is not about numbers, equations, computations or algorithms
+        it is about
+        <strong> Understanding.</strong>
+        <span> ~ Wiliam Paul Thurston</span>
+      </p>
+    </div>
   </div>
 );
 
